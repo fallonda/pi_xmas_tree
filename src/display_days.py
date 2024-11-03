b688301911547d99d4707ef8023deac9510bd485
fallonda <david.j.fallon@gmail.com>
2024-11-03 21:12:39 +0000

    Complete display_days.py

--- src/display_days.py
+++ src/display_days.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-# Script to run countdown to Christmas Day on the 
-# 7-seg display. 
-# Output will be "123 DAYS"
+# Script to run countdown to Christmas Day on the 7-seg display. 
+# Output will be e.g. "123 DAYS"
 
-# Environment setup
 from time import sleep
 import ZeroSeg.led as seg
 import random
 from datetime import date
 from utils import get_days_to_xmas, buffer_digit
+
+SEC_BETWEEN_SCROLLING_MESSAGE = 60
 
 # Setup segment display
 device_scroll = seg.sevensegment(cascaded=2)
@@ -22,50 +22,37 @@ messages = [
   "NOEL NOEL NOEL",
   "SANTA STOP HERE",
   "ROWAN IS ON THE GOOD LIST",
-  "JINGLE BELLS"
+  "JINGLE BELLS",
+  "LYNN IS THE BEST MOM"
 ]
 
-days_to_xmas = get_days_to_xmas(
-    current_date = date.today()
-)
-buffer_days_to_xmas = buffer_digit(days_to_xmas)
+def show_days_message(device, deviceId, days: str, dot_on: bool):
+    """Write to 7-seg with e.g. '123 days'"""
+    message_str = days + " days"
+    # Fill the first 7 positions.
+    # Display position is ordered 1-8 right to left. 
+    for i in range(7):
+        device.letter(deviceId, 8-i, message_str[i])
+    # For the last position (RHS, 1), add option for turning on the dot.
+    device.letter(deviceId, 1, message_str[7], dot_on)
 
- 
-# def countdown_display(device, deviceId, dot_on):
-#   """Write the digits to the display"""
-#   time_until_xmas = xmas - datetime.now()
-#   days = buffer_digit(time_until_xmas.days)
-#   hours = buffer_digit(int(time_until_xmas.seconds / 3600))
-#   mins = buffer_digit(int(time_until_xmas.seconds % 3600 / 60))
-#   device.letter(deviceId, 8, days[0])
-#   device.letter(deviceId, 7, days[1])
-#   device.letter(deviceId, 6, "-")
-#   device.letter(deviceId, 5, hours[0])
-#   device.letter(deviceId, 4, hours[1])
-#   device.letter(deviceId, 3, "-")
-#   device.letter(deviceId, 2, mins[0])
-#   device.letter(deviceId, 1, mins[1], dot_on)
-# 
-# counter_for_message = 0
-# 
-# # Main
-# try:
-#   while True:
-#     # Start countdown display
-#     countdown_display(device_static, 0, False)
-#     sleep(1)
-#     countdown_display(device_static, 0, True)
-#     sleep(1)
-#     counter_for_message += 2
-#     if counter_for_message > 180:
-#       device_scroll.show_message(random.choice(messages), delay = 0.2)
-#       counter_for_message = 0
-# 
-# except KeyboardInterrupt:
-#     device_scroll.clear()
-#     device_static.clear()
-# 
-# # End.
-# 
+# Main loop
+counter_for_intermittent_message = 0
+try:
+    while True:
+        days_to_xmas = get_days_to_xmas(date.today())
+        buffer_days_to_xmas = buffer_digit(days_to_xmas)
+        show_days_message(device_static, 0, buffer_days_to_xmas, False)
+        sleep(1)
+        show_days_message(device_static, 0, buffer_days_to_xmas, True)
+        sleep(1)
+        counter_for_intermittent_message += 2 # 2 sec passed
+        if counter_for_intermittent_message >= SEC_BETWEEN_SCROLLING_MESSAGE:
+            device_scroll.show_message(random.choice(messages), delay = 0.2)
+            counter_for_intermittent_message = 0
 
+except KeyboardInterrupt:
+    device_scroll.clear()
+    device_static.clear()
 
+# End.
